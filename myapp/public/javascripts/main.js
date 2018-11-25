428649947b565db1bc810b558b555583232c783c
ldecoster <loic.decoster@isen.yncrea.fr>
2018-11-25 10:11:57 +0100

    Construction du système de communication entre l'UI et NodeJS

--- myapp/public/javascripts/main.js
+++ myapp/public/javascripts/main.js
@@ -1,6 +1,7 @@
 (function(){
     //console.log("coucou le monde");
     ace.require("ace/ext/language_tools");
+
     var editor = ace.edit("editor");
     editor.setTheme("ace/theme/twilight");
     editor.session.setMode("ace/mode/javascript");
@@ -10,10 +11,28 @@
     //editor = document.getElementById("editor");
 
     launch_btn.addEventListener("click", function(event){
-        console.log(disp);
-        disp.innerHTML = editor.getValue();
-        console.log(editor.getValue());
-        console.log(disp);
+        event.preventDefault();
+        code = editor.getValue();
 
+        disp.innerHTML = code;
+        console.log(code);
+
+        var data = {'data' : code};
+        $.ajax({
+            type : "POST",
+            contentType : "application/json",
+            url : window.location,
+            data : JSON.stringify(data),
+            dataType : 'json',
+            success : function(data) {
+                console.log('Data renvoyée par le serveur :'); 
+                console.log(data);
+            },
+            error : function(e) {
+                alert("Error!")
+                console.log("ERROR: ", e);
+            }
+        });
     });
+
 })();
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -5,7 +5,15 @@ var router = express.Router();
 
 /* GET server page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Turtle Graphic' });
+	res.render('index', { title: 'Turtle Graphic' });
+});
+
+router.post('/', function(req, res, next) {
+	console.log('Get data : ' + JSON.stringify(req.body));
+	res.send(req.body);
+ 	//var p1 = req.body.p1; 
+ 	//console.log("p1=" + p1);
+ 	//res.render('index', { title: 'Turtle Graphic' });
 });
 
 module.exports = router;
