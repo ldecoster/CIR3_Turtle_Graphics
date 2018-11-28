ea9ef3a31dc51b23b4324f35c4ccfbd8cdd608c2
42pierre42 <paque.pierre@gmail.com>
2018-11-28 21:40:30 +0100

    correction imbrication de for() pour le rendu graphique

--- myapp/public/javascripts/main.js
+++ myapp/public/javascripts/main.js
@@ -40,31 +40,36 @@ var updateDisplay = function(command_array){
     var temp_point = new Array();
     var polygon_collection = new Array();
 
+    //création de la collection de polygones qui seront ensuite affichés
 	for(let i = 0; i < Object.keys(command_array).length; i++){
 		if(command_array[i].cmd == 'TELEPORT'){
-			
-				polygon_collection.push(temp_point);
+            
+                if(temp_point.length != 0){
+                    polygon_collection.push(temp_point);
+                }
+				
 				temp_point = new Array();
 				temp_point.push(command_array[i].val);			
 		}
 		if(command_array[i].cmd == 'MOVE'){
             temp_point.push(command_array[i].val);
-            //console.log(temp_point.length);
         }
 
-		for(let i = 0; i<polygon_collection.length;i++){
-				if(polygon_collection[i].length>=2){
-					for(let j = 0;j+1<polygon_collection[i].length;j++){
-						var s = polygon_collection[i][j];
-						var e = polygon_collection[i][j+1];
-						var line = draw_zone.line(s[0],s[1],e[0],e[1]).stroke({ width: 1, color:'#FF5500' });
-					}
-				}
-			}
+        console.log(polygon_collection.length);
+    }
+    //affichage du graphe
+    for(let i = 0; i<polygon_collection.length;i++){
+        if(polygon_collection[i].length>=2){
+            for(let j = 0;j+1<polygon_collection[i].length;j++){
+                var s = polygon_collection[i][j];
+                var e = polygon_collection[i][j+1];
+                var line = draw_zone.line(s[0],s[1],e[0],e[1]).stroke({ width: 1, color:'#FF5500' });
+            }
+        }
     }
 
-    console.log("collection de polygones :");
-    console.log(polygon_collection);
+    //console.log("collection de polygones :");
+    //console.log(polygon_collection);
 
 }
 	
